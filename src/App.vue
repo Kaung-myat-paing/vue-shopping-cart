c17a5d8ccb905140c2cb05c0eaaf45f31ce37a72
Kaungmyatpaing <kmyat5258@gmail.com>
2020-12-02 23:32:42 +0630

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app flat color="white">
+      <div class="d-inline-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-toolbar-title>Vuetify Shopping Cart</v-toolbar-title>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        
+        target="_blank"
+        text
+      >
+        <v-icon>shopping_cart</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <home />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/home";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+  
+};
+</script>
---
+++ src/components/home.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-container fluid style="background: #f2f5f8">
+    <!-- hero -->
+    <div class="container">
+      <v-carousel hide-delimiters height="255">
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
+      </v-carousel>
+    </div>
+    <!-- products -->
+    <div class="container pt-15">
+      <p class="text-h6">FEATURED PRODUCTS</p>
+      <v-row justify="center">
+        <v-col md="4" cols="12" v-for="product in productList" :key="product.id">
+          <v-card max-width="374">
+            <template slot="progress">
+              <v-progress-linear
+                color="deep-purple"
+                height="10"
+                indeterminate
+              ></v-progress-linear>
+            </template>
+
+            <v-img height="250" :src="product.image"></v-img>
+
+            <v-card-title>{{ product.name }}</v-card-title>
+
+            <v-card-actions>
+              <div class="pl-2 subtitle-1">$ {{ product.price }}</div>
+              <v-spacer></v-spacer>
+
+              <v-btn dark small color="primary" outlined>
+                <v-icon dark>add_shopping_cart</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    items: [
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+      },
+    ],
+    productList: [],
+  }),
+  created() {
+    this.getProducts();
+  },
+  methods: {
+    async getProducts() {
+      const contentful = require("contentful");
+      const client = contentful.createClient({
+        space: "wy7c2xh4rfri",
+        accessToken: "YK7gMsZ6BhjoFV_qGQvT6MMkjCWgGl68g4VWVkgOPUs",
+      });
+      try {
+        const response = await client.getEntries({
+          content_type: "clothingProducts",
+        });
+        this.productList = response.items.map((product) => {
+          const { name, price } = product.fields;
+          const { id } = product.sys;
+          const image = product.fields.image.fields.file.url;
+          return { name, price, id, image };
+        });
+        console.log("res", this.productList);
+      } catch (error) {
+        console.error();
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
